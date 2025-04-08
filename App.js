7e10869a53830cc337ff01fff02f79b38638dc9b
ChickenT-wav <matisse.ruel.1@ens.etsmtl.ca>
2025-04-08 14:24:33 -0400

    timer init

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import CirclePom from './components/TimerPom/CircleTimer';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CirclePom/>
       <StatusBar style="auto" />
     </View>
   );
